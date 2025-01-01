d7caf9bd228396edcbe78518ea6609e86a8707c4
KenshiroTM <kenshirotm@gmail.com>
2025-01-01 23:40:17 +0100

    update gameplay

--- Scenes/Game/GameCanvas.cs
+++ Scenes/Game/GameCanvas.cs
@@ -3,17 +3,50 @@ using System;
 
 public partial class GameCanvas : CanvasLayer
 {
+    [ExportSubgroup("Windows")]
+    [Export] ReferenceRect introductionWindow;
+    [Export] ReferenceRect gameWindow;
+
+    [ExportSubgroup("MenuButtons")]
     [Export] Button backToMenuBtn;
+
+    [ExportSubgroup("Introduction")]
+    [Export] RichTextLabel story;
+    [Export] Button continueBtn;
+
+    [ExportSubgroup("GameOutput")]
+    [Export] Button char1;
+    [Export] Button char2;
+    [Export] Button char3;
+    [Export] Label charlabel1;
+    [Export] Label charlabel2;
+    [Export] Label charlabel3;
+
+    [ExportSubgroup("GameInput")]
+    [Export] TextEdit playerInputText;
+    [Export] Button sendButton;
 
     public override void _Ready()
     {
         GameManager.gameState = GameManager.GameState.InGame;
 
+        // TODO: somewhere game generation
+
         backToMenuBtn.Pressed += loadMenu;
+        continueBtn.Pressed += continueStory;
+    
     }
 
+    
     public void loadMenu()
     {
         GetTree().ChangeSceneToFile(GameManager.menuScene);
     }
+
+    public void continueStory()
+    {
+        introductionWindow.Visible = false;
+        gameWindow.Visible = true;
+    }
+
 }
