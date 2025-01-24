4b49bdc9d748ba2499e638a571f0c929c8ca6221
KenshiroTM <kenshirotm@gmail.com>
2025-01-24 10:35:31 +0100

    stories

---
+++ Scenes/Stories/Story.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Story : Resource
+{
+    public string storyDesc;
+    public List<string> peopleNames;
+    public List<bool> guiltyPeople; 
+
+
+}
---
+++ Scenes/Stories/StoryLoader.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class StoryLoader : JsonRpc
+{
+    public string pathname;
+
+    public Story LoadRandomStory(pathname)
+    {
+
+    }
+}
